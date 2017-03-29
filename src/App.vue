883953baca86ea785fabba459edd2706c76e2b43
yingchao <qth545573421@gmail.com>
2017-03-29 19:01:57 +0800

    1.0.0
    创建

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="wrapper">
+    <nav class="navbar navbar-default">
+      <titlebar></titlebar>
+    </nav>
+      <tablebar></tablebar>
+  </div>
+</template>
+
+<script>
+  import Titlebar from './components/Titlebar.vue'
+  import Tablebar from './components/Tablebar.vue'
+  export default {
+    components: { 'titlebar': Titlebar, 'tablebar': Tablebar }
+  }
+</script>
